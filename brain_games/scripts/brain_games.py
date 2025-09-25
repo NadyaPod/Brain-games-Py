1200fb11e6eb2e262dd846a4e42c8bdbab87b5c1
NadyaPod <nadegda.podyacheva@gmail.com>
2025-09-25 22:38:35 +0700

    feat: greet

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
-from brain_games.scripts.tg_bot import run_tg_bot
+# from brain_games.scripts.tg_bot import run_tg_bot
+from brain_games.cli import welcome_user
 
 def main():
-    run_tg_bot()
+    # run_tg_bot()
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == '__main__':
     main()
